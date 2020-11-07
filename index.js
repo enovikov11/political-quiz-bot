7588489f63ac678455a0ff4988aa34d1bfc43662
Evgenii Novikov <enovikov11@yandex.ru>
2020-11-07 21:27:11 +0300

    ping-pong подготовленный к высокой нагрузке, без тестов

--- index.js
+++ index.js
@@ -1,22 +1,76 @@
+// https://core.telegram.org/bots/api
+
 const fs = require('fs');
 const YAML = require('yaml');
+const fetch = require('node-fetch');
 
 const { questions } = YAML.parse(fs.readFileSync('./quiz.yaml', 'utf8'))
 const api_base = process.env.QUIZBOT_API_BASE || "https://api.telegram.org/";
+const api_key = process.env.QUIZBOT_API_KEY;
 const locale = 'ru';
 const UPDATE_ERROR_WAIT = 100;
 const UPDATE_MAX_ERRORS = 100;
+const UPDATE_POLLING_INTERVAL = '60';
 
-
-let offset = '0';
+let offset = 0;
 let update_errors_count = 0;
+let chats_chains = {};
+let stat_voted = { "more equality than markets": 0, "more liberty than authority": 0, "more progress than tradition": 0, "more world than nation": 0 };
+let stat_max = { "more equality than markets": 0, "more liberty than authority": 0, "more progress than tradition": 0, "more world than nation": 0 };
+
+function makePermulation(length) {
+    return new Array(length).fill(0).map((_, i) => ({ value: Math.random(), i }))
+        .sort((a, b) => a.value > b.value ? 1 : -1).map(({ i }) => i);
+}
+
+async function on_message({ chat, text }) {
+    await fetch(api_base + 'bot' + api_key + '/sendMessage', {
+        method: 'POST',
+        body: JSON.stringify({
+            chat_id: chat.id, text: text + text,
+
+            reply_markup: {
+                keyboard: [[{ text: "1" }, { text: "2" }, { text: "3" }, { text: "4" }, { text: "5" }]]
+                // remove_keyboard: true
+            }
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    }).then(res => res.json());
+}
+
+async function on_result({ message }) {
+    if (!message.chat.id) {
+        throw new Error('Bad chat id');
+    }
+
+    if (!chats_chains[message.chat.id]) {
+        chats_chains[message.chat.id] = Promise.resolve();
+    }
+
+    chats_chains[message.chat.id].then(() => {
+        on_message(message).catch(console.error);
+    });
+}
 
 async function main() {
     while (true) {
         update_errors_count = 0;
         try {
+            const updates_result = await fetch(api_base + 'bot' + api_key + '/getUpdates', {
+                method: 'POST',
+                body: JSON.stringify({ offset, timeout: UPDATE_POLLING_INTERVAL, allowed_updates: ['message'] }),
+                headers: { 'Content-Type': 'application/json' }
+            }).then(res => res.json());
 
+            if (!updates_result.ok) {
+                throw new Error(JSON.stringify(updates_result));
+            }
 
+            offset = Math.max(...updates_result.result.map(({ update_id }) => update_id)) + 1;
+
+            for (let i = 0; i < updates_result.result.length; i++) {
+                on_result(updates_result.result[i]).catch(console.error);
+            }
         } catch (e) {
             console.error(e);
             update_errors_count++;
@@ -28,57 +82,6 @@ async function main() {
     }
 }
 
-// main().catch(console.error)
-//     // Immideatly exit if main loop is broken, throwing away background promises
-//     .then(() => process.exit(1));
-
-console.log(JSON.stringify(questions))
-
-// async function runMethod(name, params) {
-//     const fetch_result = await fetch('https://api.telegram.org/bot' + key + '/' + name, {
-//         method: 'POST',
-//         body: JSON.stringify(params),
-//         headers: { 'Content-Type': 'application/json' }
-//     }).then(res => res.json());
-//     const { result, ok } = fetch_result;
-//     if (!ok) {
-//         throw new Error(JSON.stringify(fetch_result));
-//     }
-//     return result;
-// }
-
-// function makePermulation(length) {
-//     return new Array(length).fill(0).map((_, i) => ({ value: Math.random(), i }))
-//         .sort((a, b) => a.value > b.value ? 1 : -1).map(({ i }) => i);
-// }
-
-// async function main() {
-//     while (true) {
-//         try {
-//             const messages = await runMethod('getUpdates', { offset, timeout: '60' });
-//             for (let message of messages) {
-//                 if (!message.message) {
-//                     continue;
-//                 }
-//                 try {
-//                     offset = Math.max(+offset, +message.update_id + 1);
-
-//                     if (message.message.text && message.message.chat && message.message.chat.id) {
-//                         await runMethod('sendMessage', {
-//                             chat_id: message.message.chat.id,
-//                             text: message.message.text,
-//                             reply_markup: {
-//                                 inline_keyboard: [[{ text: "1", callback_data: "1" }, { text: "2", callback_data: "2" }, { text: "3", callback_data: "3" }], [{ text: "4", callback_data: "4" }, { text: "5", callback_data: "5" }]],
-//                                 // resize_keyboard: true
-//                             }
-//                         });
-//                     }
-//                 } catch (e) {
-//                     console.error(e);
-//                 }
-//             }
-//         } catch (e) {
-//             console.error(e);
-//         }
-//     }
-// }
+main().catch(console.error)
+    // Immideatly exit if main loop is broken, throwing away background promises
+    .then(() => process.exit(1));
